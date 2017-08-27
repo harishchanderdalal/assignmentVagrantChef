d7a3eb0c89d3545e77b6d509d5940b072ad8fbc7
Harish Chander Dalal <harish.chander@hotmail.com>
2017-08-27 12:00:54 +0530

    Update jenkinsInstall.sh

--- jenkinsInstall.sh
+++ jenkinsInstall.sh
@@ -10,3 +10,4 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
+sudo sed -i '/%sudo   ALL=(ALL:ALL) ALL/ a jenkins ALL=(ALL) NOPASSWD:ALL' /etc/sudoers
